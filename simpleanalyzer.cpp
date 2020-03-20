8e0f908f7229115371ea4e1f13ca89536bf2f6b4
vladi951 <62330258+vladi951@users.noreply.github.com>
2020-03-20 23:11:38 +0200

    Update simpleanalyzer.cpp

--- simpleanalyzer.cpp
+++ simpleanalyzer.cpp
@@ -18,17 +18,35 @@ public:
     virtual ~analyzer();
     
     dEdx_Calculator* e_calc;
+    dEdx_Calculator* alpha_calc;
+    dEdx_Calculator* heavyion_calc;
+    dEdx_Calculator* helium3_calc;
+    dEdx_Calculator* triton_calc;
+    dEdx_Calculator* deuteron_calc;
+    dEdx_Calculator* p_calc;
     
 };
 
 analyzer::analyzer()
 {
     e_calc = new dEdx_Calculator("electrons", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
+    alpha_calc = new dEdx_Calculator("alpha", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
+    heavyion_calc = new dEdx_Calculator("heavyion", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
+    helium3_calc = new dEdx_Calculator("helium3", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
+    triton_calc = new dEdx_Calculator("triton", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
+    deutron_calc = new dEdx_Calculator("deutron", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
+    p_calc = new dEdx_Calculator("protons", 101, -5.05, 5.05,100, -5.05, 5.05, 1000,0.,100.);
 }
 
 analyzer::~analyzer()
 {
     delete e_calc;
+    delete alpha_calc;
+    delete heavyion_calc;
+    delete helium3_calc;
+    delete triton_calc;
+    delete deutron_calc;
+    delete p_calc;
 }
 
 void analyzer::Analyse_Energy_Record(first_rec_case_2 &rec, rec_case_2 &rec_data)
@@ -40,6 +58,42 @@ void analyzer::Analyse_Track_Record(first_rec_case_1 &rec, SimpleFlukaTrack *trk
     if (rec.JTRACK == ELECTRON)
     {
         e_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
+                              trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
+    }
+    
+     if (rec.JTRACK == HELIUM_4)
+    {
+        alpha_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
+                              trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
+    }
+    
+    if (rec.JTRACK == HEAVYION)
+    {
+        heavyion_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
+                              trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
+    }
+    
+     if (rec.JTRACK == HELIUM_3)
+    {
+        helium3_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
+                              trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
+    }
+    
+       if (rec.JTRACK == TRITON)
+    {
+        triton_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
+                              trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
+    }
+    
+     if (rec.JTRACK == DEUTERON)
+    {
+        deutron_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
+                              trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
+    }
+    
+    if (rec.JTRACK == PROTON)
+    {
+        p_calc->set_dEdx_Edep(trk->Segmets_ends[1].x, trk->Segmets_ends[1].y, trk->Segmets_ends[1].z,
                               trk->energy_deposition_events[0]/trk->total_curved_path ,trk->energy_deposition_events[0]);
     }
 }
@@ -61,16 +115,182 @@ int main(int argc, char **argv)
     int nprimary = fluread.Analyse(&an);
     TFile rfile(argv[2], "RECREATE");
     std::cout << "Primary particles: " << nprimary << std::endl;
-    an.e_calc->get_3d_Dose("Electron_Dose")->Write();
-    an.e_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Electron_Dose")->Write();
-    an.e_calc->get_2d_Dose_profile("xy",0.1,"Dose_xy_at_0_1")->Write();
-    an.e_calc->get_2d_Dose_profile("xy",0.1,"Dose_xy_at_0_1")->Write();
-    an.e_calc->get_2d_Dose_profile("xy",10.,"Dose_xy_at_10")->Write();
-    an.e_calc->get_2d_Dose_profile("xy",21.9,"Dose_xy_at_21_9")->Write();
-    an.e_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Electron_Dose_x_y0_z21_9")->Write();
-    an.e_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Electron_Dose_x_y0_z10")->Write();
-    an.e_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Electron_av_dEdx")->Write();
-    an.e_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Electron_dEdx_D")->Write();
+    
+    {// electrons
+    auto hist = an.e_calc->get_3d_Dose("Electron_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.e_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Electron_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.e_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Electron_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.e_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Electron_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.e_calc->get_1d_Dose_projection("z","Electron_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.e_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Electron_av_dEdx_prof")->Write();
+    an.e_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Electron_dEdx_D_prof")->Write();
+    }
+    
+    {//alpha
+    auto hist = an.alpha_calc->get_3d_Dose("Alpha_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.alpha_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Alpha_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.alpha_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Alpha_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.alpha_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Alpha_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.alpha_calc->get_1d_Dose_projection("z","Alpha_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.alpha_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Alpha_av_dEdx_prof")->Write();
+    an.alpha_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Alpha_dEdx_D_prof")->Write();
+    }
+    
+    { //heavyion
+    auto hist = an.heavyion_calc->get_3d_Dose("Heavyion_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.heavyion_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Heavyion_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.heavyion_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Heavyion_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.heavyion_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Heavyion_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.heavyion_calc->get_1d_Dose_projection("z","Heavyion_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.heavyion_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Heavyion_av_dEdx_prof")->Write();
+    an.heavyion_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Heavyion_dEdx_D_prof")->Write();
+    }
+    
+    { //helium3
+    auto hist = an.helium3_calc->get_3d_Dose("Helium3_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.helium3_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Helium3_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.helium3_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Helium3_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.helium3_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Helium3_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.helium3_calc->get_1d_Dose_projection("z","Helium3_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.helium3_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Helium3_av_dEdx_prof")->Write();
+    an.helium3_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Helium3_dEdx_D_prof")->Write();
+    }
+    
+    { //triton
+    auto hist = an.triton_calc->get_3d_Dose("Triton_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.triton_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Triton_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.triton_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Triton_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.triton_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Triton_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.triton_calc->get_1d_Dose_projection("z","Triton_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.triton_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Triton_av_dEdx_prof")->Write();
+    an.triton_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Triton_dEdx_D_prof")->Write();
+    }
+    
+    {// deutron
+    auto hist = an.deutron_calc->get_3d_Dose("Deutron_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.deutron_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Deutron_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.deutron_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Deutron_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.deutron_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Deutron_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.deutron_calc->get_1d_Dose_projection("z","Deutron_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.deutron_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Deutron_av_dEdx_prof")->Write();
+    an.deutron_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Deutron_dEdx_D_prof")->Write();
+    }
+    
+     {// protons
+    auto hist = an.p_calc->get_3d_Dose("Proton_Dose");
+    hist->Scale(1/nprimary);
+    hist->  Write();
+    
+    auto hist1 = an.p_calc->get_1d_Dose_profile("z","x",0.,"y",0.,"Proton_Dose_z_prof");
+    hist1-> Scale(1/nprimary);
+    hist1-> Write();  
+    
+    auto hist2 = an.p_calc->get_1d_Dose_profile("x","y",0.,"z",21.9,"Proton_Dose_x_y0_z21_9_prof");
+    hist2-> Scale(1/nprimary);
+    hist2-> Write();
+    
+    auto histo3 = an.p_calc->get_1d_Dose_profile("x","y",0.,"z",10.,"Proton_Dose_x_y0_z10_prof");
+    hist3-> Scale(1/nprimary);
+    hist3-> Write();
+    
+    auto hist4 = an.p_calc->get_1d_Dose_projection("z","Proton_Dose_z_proj");
+    hist4-> Scale(1/nprimary);
+    hist4-> Write();
+    
+    an.p_calc->get_1d_av_dEdx_profile("z","x",0.,"y",0.,"Proton_av_dEdx_prof")->Write();
+    an.p_calc->get_1d_dEdx_D_profile("z","x",0.,"y",0.,"Proton_dEdx_D_prof")->Write();
+    }
+    
     rfile.Close();            
     return 0;
 }
